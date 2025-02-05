798fda0c0d2e8288a59d34c675c5cce88fc386e2
abhirambj <abhiram.bj@gmail.com>
2025-02-05 14:31:44 -0600

    modified app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,15 @@ import NotFound from "./pages/NotFound";
 import Resume from "./pages/Resume";
 
 function App() {
-  return (
-    <>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/resume" element={<Resume />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </>
-  );
+	return (
+		<>
+			<Routes>
+				<Route path="/portfolio" element={<Main />} />
+				<Route path="/portfolio/resume" element={<Resume />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+		</>
+	);
 }
 
 export default App;
