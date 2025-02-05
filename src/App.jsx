d3d037e1e7010a65b4e0e4cf70021c641935654b
abhirambj <abhiram.bj@gmail.com>
2025-02-05 14:40:20 -0600

    modified routes for proper deployment

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
 	return (
 		<>
 			<Routes>
-				<Route path="/portfolio" element={<Main />} />
+				<Route path="/portfolio/" element={<Main />} />
 				<Route path="/portfolio/resume" element={<Resume />} />
 				<Route path="*" element={<NotFound />} />
 			</Routes>
